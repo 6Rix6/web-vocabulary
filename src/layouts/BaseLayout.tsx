0434da869b6f701079a3f8ace7c0f3e755e0ce9b
6Rix6 <tesimariku0428@outlook.jp>
2025-08-23 01:59:47 +0900

    格子背景を追加

---
+++ src/layouts/BaseLayout.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export default function BaseLayout({ children }: Props) {
+  return (
+    <div className="min-h-screen bg-fixed bg-[linear-gradient(to_right,#80808033_1px,transparent_1px),linear-gradient(to_bottom,#80808033_1px,transparent_1px)] bg-[size:70px_70px] antialiased">
+      {children}
+    </div>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import BaseLayout from "./layouts/BaseLayout";
 import Home from "./pages/Home";
 import Vocabulary from "./pages/Vocabulary";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
@@ -9,8 +10,22 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/vocabulary" element={<Vocabulary />} />
+        <Route
+          path="/"
+          element={
+            <BaseLayout>
+              <Home />
+            </BaseLayout>
+          }
+        />
+        <Route
+          path="/vocabulary"
+          element={
+            <BaseLayout>
+              <Vocabulary />
+            </BaseLayout>
+          }
+        />
       </Routes>
     </BrowserRouter>
   </StrictMode>
